e3a76440166285e7f952d937212cf1f069212d34
CuberShroom <138808967+CuberShroom@users.noreply.github.com>
2024-11-24 00:18:03 +0500

    Add files via upload

---
+++ Beta_parser -/main.py
@@ -0,0 +1,53 @@
+import asyncio
+import logging
+import sys
+from os import getenv
+from parser_run import Check_torf, quantity,price,secid, updatetime
+from aiogram import Bot, html
+from aiogram.client.default import DefaultBotProperties
+from argparse import ArgumentParser
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+import time
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+
+
+
+while True:
+    async def main():
+        global Check_torf, quantity, price, secid, updatetime
+        token = ''
+        chat_id = '@TestChannelForinvestBot'
+        message = f"""
+            🟢 #{secid}
+    ПОКУПКА  {price} руб.
+    ОБЪЁМ {quantity}
+    ВРЕМЯ {updatetime}
+    ___________
+            """
+
+
+        
+    
+        async with Bot(
+            token=token,
+            default=DefaultBotProperties(
+                
+            ),
+        ) as bot:
+            if Check_torf== True:
+
+                await bot.send_message(chat_id=chat_id, text=message)
+                Check_torf=False
+                print(Check_torf, secid)
+                
+            else:
+                print(Check_torf)
+        
+    if __name__ == "__main__":
+        asyncio.run(main())
+
+
+    
+    
